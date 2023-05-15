ab09a7303a7280580a6bf16a82cc8674e48d2e68
wjunjie01 <junjiework48@gmail.com>
2023-05-15 18:15:39 +0800

    Fixed the movement, added in Tilemaps
    Additionally changed the project scene size. Added tilemap for player to stand on

--- TheHeist_godot/player.gd
+++ TheHeist_godot/player.gd
@@ -1,13 +1,15 @@
 extends CharacterBody2D
 
-const GRAVITY = 100.0
+const GRAVITY = 800.0
 const WALK_SPEED = 200.0
-const JUMP = 50
+const JUMP = 600
 
 func _physics_process(delta):
 	velocity.y += GRAVITY * delta
 	velocity.x = 0
-	if Input.is_action_just_pressed("action_jump"):
+	#expression return a boolean expression
+	var is_falling = velocity.y > 0.0 and not is_on_floor()
+	if Input.is_action_just_pressed("action_jump") and is_on_floor():
 		velocity.y -= JUMP
 	if Input.is_action_pressed("move_left"):
 		velocity.x = -WALK_SPEED
