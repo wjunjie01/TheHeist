f9d84550127cdc7f7ac0d821055c940b2cccba6f
wjunjie01 <junjiework48@gmail.com>
2023-05-16 08:43:24 +0800

    Added in a new file to test

--- TheHeist_godot/player.gd
+++ TheHeist_godot/player.gd
@@ -3,6 +3,23 @@ extends CharacterBody2D
 const GRAVITY = 800.0
 const WALK_SPEED = 200.0
 const JUMP = 600
+#var current_rope_length
+#var rope_length = 500
+#var hook_pos = Vector2.ZERO
+#var hooked
+
+
+#func hook():
+#	$Raycast.look_at(get_global_mouse_position())
+#	if Input.is_action_just_pressed("left_click"):
+#		hook_pos = get_hook_pos()
+#		if hook_pos:
+#			hooked = true
+#			#global position is from player
+#			current_rope_length = global_position.distance_to(hook_pos)
+#
+#func get_hook_pos():
+#
 
 func _physics_process(delta):
 	velocity.y += GRAVITY * delta
