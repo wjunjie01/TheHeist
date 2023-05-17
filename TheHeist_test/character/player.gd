6d8c78eb99938e5c8ea052edb0434c899a153218
wjunjie01 <junjiework48@gmail.com>
2023-05-17 17:01:48 +0800

    added in script for chain

--- TheHeist_test/character/player.gd
+++ TheHeist_test/character/player.gd
@@ -7,11 +7,17 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var direction
+var screen_size = Vector2.ZERO
 var animation_locked : bool = false #if == true an animation is playing 
+
+func _ready():
+	screen_size = get_viewport_rect().size 
 
 func _physics_process(delta):
 	# Get the input direction and returns a float +ve if right -ve if left
 	direction = Input.get_axis("move_left", "move_right")
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
 	#reset animation locked each frame
 	animation_locked = false
 	#add gravity
@@ -46,3 +52,4 @@ func update_animation():
 			$AnimatedSprite2D.play("run")
 		if direction == 0 and is_on_floor():
 			$AnimatedSprite2D.play("idle")
+
