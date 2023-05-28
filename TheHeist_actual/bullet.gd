c273bb4961ba40fd68c176f6c758e87a2f01bbeb
cheeseong2001 <cheeseong2001@gmail.com>
2023-05-29 00:03:52 +0800

    Bug fixes

--- TheHeist_actual/bullet.gd
+++ TheHeist_actual/bullet.gd
@@ -6,5 +6,5 @@ func _physics_process(delta):
 	position += transform.x * speed * delta * direction
 
 func _on_Bullet_body_entered(body):
-	body.queue_free()
-	queue_free()
+	get_tree().reload_current_scene()
+	
