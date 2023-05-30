b10bd336d0457b1af29a04105065f6a3c9c1f88f
wjunjie01 <junjiework48@gmail.com>
2023-05-30 11:48:05 +0800

    Working on the new grappling_hook not done yet

---
+++ TheHeist_actual/hook_point.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+var cursor = load("res://art/cross_hair_1.png")
+var old_cursor_shape = load("res://art/crosshair007.png")
+
+func _on_mouse_entered():
+	Input.set_custom_mouse_cursor(cursor, Input.CURSOR_ARROW, Vector2(16,16))
+
+ 
+
+func _on_mouse_exited():
+	Input.set_custom_mouse_cursor(old_cursor_shape, Input.CURSOR_ARROW,Vector2(16,16))
+
