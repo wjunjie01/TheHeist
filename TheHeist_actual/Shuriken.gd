42aa9371b2ef259cbc725392dcb7cd0c861ddfd4
cheeseong2001 <cheeseong2001@gmail.com>
2023-06-22 17:17:59 +0800

    Added new projectile shuriken
    - Aim and press E to shoot
    
    -Also moved pointer to player node itself

---
+++ TheHeist_actual/Shuriken.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+var velocity = Vector2.ZERO
+var speed = 20
+var fired = false
+
+		
+func _physics_process(delta):
+	position += velocity.normalized() * speed
+	if velocity != Vector2.ZERO:
+		$Sprite2D.rotation += 30
+
+
+
+func _on_body_entered(body):
+	if !fired and body.is_in_group("player"):
+		body.has_shuriken = true
+		body.has_trap = false
+		queue_free()
+	
+	elif fired and body.is_in_group("enemy"):
+		body.is_dead = true
+		queue_free()
+	queue_free()
