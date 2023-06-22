97129131394024044329267b5dc7b653ef3abb6e
wjunjie01 <junjiework48@gmail.com>
2023-06-22 14:10:55 +0800

    Revert "Edited the whole flow of game to add pause menu"
    This reverts commit ff0f896b7e6a7ee7dd8fc2b3019a5b706dfcb038.

--- TheHeist_actual/transition/level_1_2.gd
+++ TheHeist_actual/transition/level_1_2.gd
@@ -9,10 +9,6 @@ func _on_body_entered(body: PhysicsBody2D):
 
 func _process(delta):
 	if entered == true:
-		var next_level = preload("res://level_3.tscn").instantiate()
-		get_parent().get_parent().add_child(next_level)
-#		SceneTransition.change_scene_clouds('res://level_3.tscn')
+		SceneTransition.change_scene_clouds('res://level_3.tscn')
 		entered = false
 		first_time = false
-		get_parent().free()
-#		self.free()
