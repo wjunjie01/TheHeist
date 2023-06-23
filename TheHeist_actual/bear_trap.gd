74700f72b668d7dc107406df8f0879b33c5d5899
cheeseong2001 <cheeseong2001@gmail.com>
2023-06-23 19:18:14 +0800

    removed corrupted and unused files

--- TheHeist_actual/bear_trap.gd
+++ TheHeist_actual/bear_trap.gd
@@ -1,13 +1,9 @@
 extends Area2D
 
 var laid = false
+
 func _ready():
 	$AnimationPlayer.play("Lay Trap")
-
-
-func _physics_process(delta):
-	pass
-
 
 func _on_body_entered(body):
 	if laid and body.is_in_group("enemy"):
@@ -16,9 +12,8 @@ func _on_body_entered(body):
 
 	elif not laid and body.is_in_group("player"):
 		queue_free()
-		body.has_trap = true
+		body.has_trap = true  
 		
-
 
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "Trap":
