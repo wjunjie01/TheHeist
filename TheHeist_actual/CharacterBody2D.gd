8eeda91e070bcdb5be2c5d280e0acd0bd50bb728
wjunjie01 <junjiework48@gmail.com>
2023-07-13 17:35:49 +0800

    trying "deleting and cloning" meanwhile some boss testing p1

---
+++ TheHeist_actual/CharacterBody2D.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+const SPEED = 0.1
+
+@export var player: Node2D
+@onready var nav_agent := $NavigationAgent2D 
+
+func _physics_process(delta: float) -> void:
+	#the best shortest direction to go
+	var dir = to_local(nav_agent.get_next_path_position().normalized())
+	velocity = dir * SPEED
+	
+	move_and_slide()
+	
+func makepath() -> void:
+	nav_agent.target_position = player.global_position
+	#print(nav_agent.target_position	)
+
+func _on_timer_timeout():
+	makepath()
