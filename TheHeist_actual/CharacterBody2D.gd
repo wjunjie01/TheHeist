fb1c41838a23c0182b64d5716de91245d8f3b6a6
wjunjie01 <junjiework48@gmail.com>
2023-07-13 18:22:55 +0800

    failed boss

--- TheHeist_actual/CharacterBody2D.gd
+++ TheHeist_actual/CharacterBody2D.gd
@@ -10,11 +10,11 @@ func _physics_process(delta: float) -> void:
 	var dir = to_local(nav_agent.get_next_path_position().normalized())
 	velocity = dir * SPEED
 	
+	
 	move_and_slide()
 	
 func makepath() -> void:
 	nav_agent.target_position = player.global_position
-	#print(nav_agent.target_position	)
 
 func _on_timer_timeout():
 	makepath()
--- TheHeist_actual/boss2.gd
+++ TheHeist_actual/boss2.gd
@@ -1,15 +1,29 @@
 extends CharacterBody2D
 
-const speed = 20
+const speed = 200
+const GRAVITY = 800
 
 @export var player: Node2D
 @onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
 
 func _physics_process(delta: float) -> void:
 	var dir = to_local(nav_agent.get_next_path_position()).normalized()
+	velocity.x = dir.x * speed
 	
-	velocity = dir * speed
+	if is_on_wall():
+		print('hi')
+		velocity.y = -400
+		
+	if is_on_floor():
+		print("on floor")
+		velocity.y = 0
+	else:
+		velocity.y = GRAVITY * delta
 	move_and_slide()
+
+func jump():
+	print('jump')
+	velocity.y = -400
 
 func makepath() -> void:
 	nav_agent.target_position = player.global_position
